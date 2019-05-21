38b8f3f5c98b8fd285e69296f9e9aba75abb5110
francesco.calzolai <francesco.calzolai@lloydsbanking.com>
2019-05-21 15:49:55 +0100

    Refactor

--- src/test/java/com/lloyds/lazytester/FeatureRunnerTest.java
+++ src/test/java/com/lloyds/lazytester/FeatureRunnerTest.java
@@ -1,6 +1,7 @@
-package com.lloyds.lazytester.model;
+package com.lloyds.lazytester;
 
-import com.lloyds.lazytester.FeatureRunner;
+import com.lloyds.lazytester.model.Feature;
+import com.lloyds.lazytester.model.Utils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
