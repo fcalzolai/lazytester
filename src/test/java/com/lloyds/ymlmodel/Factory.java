bfcdf7f56d15a66c2efc72f8717e6c0d9fed601c
francesco.calzolai <francesco.calzolai@lloydsbanking.com>
2019-05-21 09:39:27 +0100

    Modify first.yml file

--- src/test/java/com/lloyds/ymlmodel/Factory.java
+++ src/test/java/com/lloyds/ymlmodel/Factory.java
@@ -1,44 +1,29 @@
 package com.lloyds.ymlmodel;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lloyds.utils.Utils;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
 public class Factory {
 
-    private static final String PATH = "yaml/steps.yaml";
+    private static final String PATH1 = "yaml/steps.yaml";
     private static final String PATH2 = "yaml/anchor.yaml";
 
     public static void main(String[] args) throws IOException {
-        URL is, is2;
-        Yaml yaml;
-        Steps step;
-        ObjectMapper mapper;
+        buildAndPrint(PATH1);
+        buildAndPrint(PATH2);
+    }
 
-//        is = Utils.class.getClassLoader().getResource(PATH);
-//        mapper = new ObjectMapper(new YAMLFactory());
-//        step = mapper.readValue(is, Steps.class);
-//        System.out.println(step);
-//
-//        yaml = new Yaml();
-//        Object load = yaml.load(new FileReader(is.getPath()));
-//        System.out.println(load);
-//
-//        is2 = Utils.class.getClassLoader().getResource(PATH2);
-//        Object load2 = yaml.load(new FileReader(is2.getPath()));
-//        System.out.println(load2);
-        is = Utils.class.getClassLoader().getResource(PATH2);
-
-        yaml = new Yaml();
+    private static void buildAndPrint(String path) throws FileNotFoundException {
+        URL is = Utils.class.getClassLoader().getResource(path);
+        Yaml yaml = new Yaml();
         Object load = yaml.load(new FileReader(is.getPath()));
         System.out.println("\n\n++++++++++++++++++");
+        System.out.println("File: " + path);
         System.out.println(load);
-
-//        mapper = new ObjectMapper(new YAMLFactory());
-//        System.out.println(mapper.readValue(is, Steps.class));
     }
 }
