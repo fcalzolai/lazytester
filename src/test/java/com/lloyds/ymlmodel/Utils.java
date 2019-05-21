aa4e96e18718b53cd7a27a5b808505742cf4d11c
francesco.calzolai <francesco.calzolai@lloydsbanking.com>
2019-05-21 11:06:17 +0100

    Add Anchor tests

--- src/test/java/com/lloyds/ymlmodel/Utils.java
+++ src/test/java/com/lloyds/ymlmodel/Utils.java
@@ -7,11 +7,12 @@ import java.io.StringReader;
 
 public class Utils {
 
-    public static void parse(String toParse, Class clazz) {
+    public static <T> T parse(String toParse, Class<T> clazz) {
         StringReader sr = new StringReader(toParse);
         Yaml yaml = new Yaml(new Constructor(clazz));
-        Object load = yaml.load(sr);
+        T load = yaml.load(sr);
         System.out.println("\n\n++++++++++++++++++");
         System.out.println(load);
+        return load;
     }
 }
