5a074b1458036055f3492b9c6f3d1c7032f72d76
francesco.calzolai <francesco.calzolai@lloydsbanking.com>
2019-07-09 16:38:09 +0100

    Add test for POST

--- src/test/java/com/lloyds/lazytester/runner/AnnotationRunnerTest.java
+++ src/test/java/com/lloyds/lazytester/runner/AnnotationRunnerTest.java
@@ -5,10 +5,11 @@ import org.junit.runner.RunWith;
 @RunWith(AnnotationRunner.class)
 @YamlUnitTestCase(
 //        features = {".*runner.*\\.yaml"}
-        features = {".*/runner/.*yaml"}
+        features = {".*/runner/*.yaml"}
 )
 public class AnnotationRunnerTest {
 
+    // Feature examples using both glob and regex:
     // "glob:**/yaml/**/*.yaml"
     // "glob:**/*.yaml"
     // "glob:**/runner/*.yaml"
