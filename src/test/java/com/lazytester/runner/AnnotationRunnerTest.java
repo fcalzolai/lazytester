565cbb0c613933647bcd4b850b089e50cea0f9f9
Francesco <francesco.calzolai@gmail.com>
2020-04-18 18:49:01 +0100

    Fix post issue

--- src/test/java/com/lazytester/runner/AnnotationRunnerTest.java
+++ src/test/java/com/lazytester/runner/AnnotationRunnerTest.java
@@ -2,14 +2,17 @@ package com.lazytester.runner;
 
 import org.junit.runner.RunWith;
 
-// Feature examples using both glob and regex:
-// "glob:**/yaml/**/*.yaml"
-// "glob:**/*.yaml"
-// "glob:**/runner/*.yaml"
-// "regex:.*/runner/.*yaml"
-// ".*/runner/.*yaml"
 @YamlUnitTestCase(
+
+        // Feature examples using both glob and regex:
+        // "glob:**/yaml/**/*.yaml"
+        // "glob:**/*.yaml"
+        // "glob:**/runner/*.yaml"
+        // "regex:.*/runner/.*yaml"
+        // ".*/runner/.*yaml"
+
 //        features = {".*runner.*\\.yaml"}
+//        features = {".*/runner/feature.yaml"}
         features = {".*/runner/.*yaml"}
 )
 @RunWith(AnnotationRunner.class)
